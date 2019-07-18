dba5865a68305e550628fd5d293aa20acc928f51
Martin Trummer <martin.trummer@tmtron.com>
2019-07-18 08:51:50 +0200

    generated client app

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'intellijweb400025-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client';
+}
